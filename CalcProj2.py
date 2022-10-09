1d9e1d939df957affe32e6583e65587f82857432
PanGames <109430498+panbrawl91@users.noreply.github.com>
2022-10-09 16:57:27 +0300

    Add files via upload

---
+++ CalcProj2.py
@@ -0,0 +1,25 @@
+rdr = 1
+dj = 6
+print(f" write Answer - {rdr} + {dj}")
+Answer = int(input("Answer"))
+print(f"YR Answ - {Answer == rdr + dj}")
+print(5 > 1)
+# Comment Приыет
+print("xlx")
+hg_1 = 166
+j_2 = 2
+print(hg_1 - j_2)
+print("8" + str(0))
+my_money = int(input("Сколько ты хочешь заработать каждую наносекунду?\n"))
+tinkoff_bank = my_money * 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+print(f"Вы накопите: {tinkoff_bank}$ денег!")
+my_bank = "Tinkoff is best bank!"
+print(my_bank)
+my_csh = int(input("сколько тебе вернется кешбека от Тинькофф?"))
+cash_back = my_csh * 99910191209028072892089038902310289239803290823809129831082380927280
+print(f"Тебе вернется: {cash_back}$ !")
+print("Спасибо, Тинькофф Банк!")
+hh_l = "Lolx"
+print(hh_l[0])
+print(len(hh_l))
+print(hh_l[0:3])
